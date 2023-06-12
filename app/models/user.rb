409393e7d84ace4100594dc0815d4108df9b1e3b
Yazeed <dee3ayahmed25@gmail.com>
2023-06-12 13:14:51 +0300

    defined two methods in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :products, through: :reviews
 
-    end
+    def favorite_product
+    self.reviews.order(:star_rating).last
+  end
+
+  def remove_reviews(product)
+    self.reviews.where(product: product).destroy_all
+  end
+end
 
